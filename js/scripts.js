434a38c41470bbf1d932be578f4436a992494014
Mac-ziis <macg286@gmail.com>
2023-06-11 18:26:55 -0700

    Add a random number generator. the css, js, and html to style and structure the code

--- js/scripts.js
+++ js/scripts.js
@@ -17,4 +17,24 @@ document.getElementById("default").addEventListener("click", function onclick() 
   document.body.style.fontSize = "20px";
 });
 
+
+// var link = document.getElementById('getNumber');
+// link.onclick = getNumber;
+
+// function getNumber() {
+//   var minNumber = 0;
+//   var maxNumber = 100;
+//   var randomNumber = Math.floor(Math.random() * (maxNumber + 1) + minNumber);
+//   $('#myNumber').html(randomNumber);
+//   return false;
+// };
+
+let generate = document.getElementById('btn');
+generate.addEventListener('click', () => {
+    let min = document.getElementById('min').value;
+    let max = document.getElementById('max').value;
+    let random_num= Math.floor(Math.random()*(max-min) + +min);
+    document.getElementById('output').innerHTML = ` =  ${random_num}`;
+})    
+
 }
